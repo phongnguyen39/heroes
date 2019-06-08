fbea3dd20e17d12617188b9157106ad5cc8f6056
phong_nguyen <phong.nguyen39@gmail.com>
2019-06-08 12:43:32 -0700

    heroes first deployment

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 2, name: 'Batman'},
+    {id: 3, name: 'Superman'},
+    {id: 4, name: 'Spiderman'},
+    {id: 5, name: 'Wolverine'},
+    {id: 6, name: 'Rogue'},
+    {id: 7, name: 'Gambit'},
+    {id: 8, name: 'Cyclops'},
+    {id: 9, name: 'Magneto'},
+]
